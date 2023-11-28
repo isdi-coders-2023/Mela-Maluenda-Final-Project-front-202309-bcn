69f4cab139c7422c005f64d205ac1c7bea68309b
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-28 22:07:35 +0100

    Add colors and typography main theme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,24 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    error: "#7c1404",
+    lighter: "#f3eedd",
+    accentLight: "#fcd35a",
+    accentDark: "#465e4f",
+    backgroundLight: "#688086",
+    backgroundDark: "#384045",
+    mainText: "#213327",
+  },
+
+  typography: {
+    mainFontFamily: "'Playfair Display', serif",
+    regularFontFamily: "'Josefin Sans', sans-serif",
+    mainSize: "3rem",
+    titleSize: "2.2rem",
+    secondaryTitleSize: "1.5rem",
+    regularSize: "1,125rem",
+  },
+};
+
+export default mainTheme;
