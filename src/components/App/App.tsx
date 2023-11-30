2cf2efd36f83257cf75d1cb2d3e823bb3151d6e4
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-30 21:12:26 +0100

    Add streetphotography home page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
+import HomePage from "../../pages/HomePage/HomePage";
 import Header from "../Header/Header";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 const App = (): React.ReactElement => {
   return (
     <main>
-      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={""} />
       </Routes>
+      <Header />
+      <HomePage />
     </main>
   );
 };
