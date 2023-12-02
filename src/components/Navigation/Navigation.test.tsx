a602dcaf9ac9394583967292684a3a147f86516f
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-02 01:08:09 +0100

    Modify navigation test including link role

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,19 +1,19 @@
+import { screen } from "@testing-library/react";
 import photosMock from "../../mocks/photosMock";
 import customRender from "../../testUtils/customRender";
 import Navigation from "./Navigation";
-import { screen } from "@testing-library/react";
 
 describe("Given a navigation component", () => {
   describe("When it rendered", () => {
     const mockContest = photosMock;
     customRender(<Navigation />, mockContest);
 
-    test("Then it should show a list with a Home name", () => {
-      const expectedLiText = "Home";
+    test("Then it should show a 'home' link", () => {
+      const expectedText = "home";
 
-      const liText = screen.getByText(expectedLiText);
+      const expectedHomeLink = screen.getByRole("link", { name: expectedText });
 
-      expect(liText).toBeInTheDocument();
+      expect(expectedHomeLink).toBeInTheDocument();
     });
   });
 });
