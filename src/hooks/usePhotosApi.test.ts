d611daef12e9e46386831d5b33d37080b28bbffd
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-03 14:44:23 +0100

    Fix setup tests

--- src/hooks/usePhotosApi.test.ts
+++ src/hooks/usePhotosApi.test.ts
@@ -3,8 +3,8 @@ import photosMock from "../mocks/photosMock/photosMock";
 import usePhotosApi from "./usePhotosApi";
 
 describe("Given a usePhotosApi hook", () => {
-  describe("when it gets the photos list information", () => {
-    test("Then it should show a list of photos", async () => {
+  describe("when it is called", () => {
+    test("Then it should return the information of a list of photos", async () => {
       const expectedPhotos = photosMock;
 
       const {
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,22 +1,7 @@
 import "@testing-library/jest-dom";
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./mocks/node";
-import { PhotosStructure } from "./store/types";
-import photosMock from "./mocks/photosMock/photosMock";
 
-export let mockPhotos: PhotosStructure[] = photosMock;
-
-beforeAll(() => {
-  mockPhotos = photosMock;
-  server.listen();
-});
-
-afterEach(() => {
-  mockPhotos = photosMock;
-  server.listen();
-});
-
-afterAll(() => {
-  mockPhotos = photosMock;
-  server.listen();
-});
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
