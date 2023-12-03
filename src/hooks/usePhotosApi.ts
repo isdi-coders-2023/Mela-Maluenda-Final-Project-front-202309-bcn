49a54ef9e2c8da71f883a4bc61e365e35ace76c9
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-03 13:39:47 +0100

    Adapt dispatch use effects to get photos from api

--- src/hooks/usePhotosApi.ts
+++ src/hooks/usePhotosApi.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { PhotosStructure } from "../store/types";
+import { useCallback } from "react";
 
 const usePhotosApi = () => {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-  const getPhotosApi = async (): Promise<PhotosStructure> => {
-    const { data: photos } = await axios.get("/photos");
+  const getPhotosApi = useCallback(async () => {
+    const { data: photos } = await axios.get<{
+      photos: PhotosStructure[];
+    }>("/photos");
+
     return photos;
-  };
+  }, []);
   return { getPhotosApi };
 };
 
