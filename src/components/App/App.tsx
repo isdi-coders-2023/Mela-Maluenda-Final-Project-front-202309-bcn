296b825860d944abfed2f81b3805eb80a2696044
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-04 21:33:05 +0100

    Add loading ui state to app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import HomePage from "../../pages/HomePage/HomePage";
 import Header from "../Header/Header";
 import { Route, Routes, Navigate } from "react-router-dom";
+import Loading from "../Loading/Loading";
+import { useAppSelector } from "../../store/hooks";
 
 const App = (): React.ReactElement => {
+  const uiState = useAppSelector((state) => state.uiState);
+
   return (
     <>
+      {uiState.isLoading && <Loading />}
       <Header />
       <main>
         <Routes>
