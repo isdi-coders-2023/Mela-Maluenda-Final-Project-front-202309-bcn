628f042727fd37ed89d70bd1bc5616f21a58a7c4
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-07 22:19:26 +0100

    Add toast container to app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@ import Header from "../Header/Header";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import { useAppSelector } from "../../store/hooks";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = (): React.ReactElement => {
   const uiState = useAppSelector((state) => state.uiState);
@@ -11,6 +13,17 @@ const App = (): React.ReactElement => {
     <>
       {uiState.isLoading && <Loading />}
       <Header />
+      <ToastContainer
+        position="top-center"
+        autoClose={8000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="light"
+      />
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
