4a8b3e8019cbaa310fac04755cbe2f5b2e627beb
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-08 18:09:17 +0100

    Add use state photo without id to aply in on change inputs

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,18 +1,57 @@
+import { useState } from "react";
+import { PhotStructureWithoutId } from "../../store/types";
 import Button from "../Button/Button";
 import FormStyled from "./FormStyled";
 
 const Form = (): React.ReactElement => {
+  const emptyPhoto: PhotStructureWithoutId = {
+    title: "",
+    author: "",
+    year: "",
+    location: "",
+    publicSpace: "",
+    category: "Long shot",
+    portfolioUrl: "",
+    whatMadeYouClick: "",
+    photoUrl: "",
+  };
+
+  const [newPhoto, setNewPhoto] = useState<PhotStructureWithoutId>(emptyPhoto);
+
+  const onChangeData = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+  ) => {
+    setNewPhoto({
+      ...newPhoto,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
     <FormStyled>
       <h2 className="form__title">ADD your PHOTO</h2>
       <label className="form__label" htmlFor="title">
         Title:
       </label>
-      <input className="form__input" required type="text" id="title" />
+      <input
+        className="form__input"
+        required
+        type="text"
+        id="title"
+        onChange={onChangeData}
+      />
       <label className="form__label" htmlFor="author">
         Author:
       </label>
-      <input className="form__input" required type="text" id="author" />
+      <input
+        className="form__input"
+        required
+        type="text"
+        id="author"
+        onChange={onChangeData}
+      />
       <label className="form__label" htmlFor="date">
         Month and year:
       </label>
@@ -23,20 +62,37 @@ const Form = (): React.ReactElement => {
         id="date"
         min="2000-01"
         max="2023-12"
+        onChange={onChangeData}
       />
       <label className="form__label" htmlFor="location">
         Location:
       </label>
-      <input className="form__input" required type="text" id="location" />
+      <input
+        className="form__input"
+        required
+        type="text"
+        id="location"
+        onChange={onChangeData}
+      />
       <label className="form__label" htmlFor="space">
         Public space:
       </label>
-      <input className="form__input" required type="text" id="space" />
+      <input
+        className="form__input"
+        required
+        type="text"
+        id="space"
+        onChange={onChangeData}
+      />
       <div className="form__category">
         <label className="form__label form__label--select" htmlFor="category">
           Category:
         </label>
-        <select className="form__input form__input--select" id="category">
+        <select
+          className="form__input form__input--select"
+          id="category"
+          onChange={onChangeData}
+        >
           <option value="">Long shot</option>
           <option value="">Medium shot</option>
         </select>
@@ -44,7 +100,13 @@ const Form = (): React.ReactElement => {
       <label className="form__label" htmlFor="portfolio">
         Portfolio url:
       </label>
-      <input className="form__input" required type="url" id="portfolio" />
+      <input
+        className="form__input"
+        required
+        type="url"
+        id="portfolio"
+        onChange={onChangeData}
+      />
       <label className="form__label" htmlFor="description">
         What made you click:
       </label>
@@ -52,11 +114,18 @@ const Form = (): React.ReactElement => {
         className="form__description"
         id="description"
         placeholder="A few lines explaining something about how did you feel or thought when you took that image."
+        onChange={onChangeData}
       ></textarea>
       <label className="form__label" htmlFor="photo">
         Add the image url:
       </label>
-      <input className="form__input" required type="url" id="photo" />
+      <input
+        className="form__input"
+        required
+        type="url"
+        id="photo"
+        onChange={onChangeData}
+      />
       <div className="form__button">
         <Button
           className="button button__form"
