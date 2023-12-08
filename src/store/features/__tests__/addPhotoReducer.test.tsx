7373839d636a98a35de3be3b5dd67e05bc167f0f
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-08 20:30:31 +0100

    Add photo reducer test

---
+++ src/store/features/__tests__/addPhotoReducer.test.tsx
@@ -0,0 +1,22 @@
+import { photoAddMock, photosMock } from "../../../mocks/photosMock/photosMock";
+import { PhotosStructure } from "../../types";
+import { addPhotoActionsCreator, photoReducer } from "../photosSlice";
+import PhotosStateStructure from "../types";
+
+describe("Given a addPhoto miniredcer", () => {
+  describe("When it receives a list of photos and a new photo Ghost witout id", () => {
+    test("Then it should return the same list of photos with the photo Ghost with id in it", () => {
+      const mockList = photosMock;
+      const mockaddPhoto = photoAddMock;
+      const initialState: PhotosStateStructure = { photos: mockList };
+      const newPhoto: PhotosStructure = mockaddPhoto[3];
+
+      const currentPhotosState = photoReducer(
+        initialState,
+        addPhotoActionsCreator(newPhoto),
+      );
+
+      expect(currentPhotosState.photos).toStrictEqual(mockaddPhoto);
+    });
+  });
+});
