dabfb196f712be657772be661fb1aad9dd088d26
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-08 20:49:50 +0100

    Add form test to test input fields

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,6 +1,7 @@
+import userEvent from "@testing-library/user-event";
 import { customRender } from "../../testUtils/customRender";
 import Form from "./Form";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 
 describe("Given a Form component", () => {
   describe("When it is rendered", () => {
@@ -12,6 +13,21 @@ describe("Given a Form component", () => {
       const inputText = screen.getByLabelText(expectedLabel);
 
       expect(inputText).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user types in all the text input fields", () => {
+    test("Then it should show the new text in the fields", async () => {
+      const expectedInput = "Enrique Muda";
+
+      customRender(<Form />);
+
+      const labelText = screen.getByLabelText("Author:");
+
+      await userEvent.type(labelText, expectedInput);
+
+      const inputText = screen.getByDisplayValue(expectedInput);
+      await waitFor(() => expect(inputText));
     });
   });
 });
