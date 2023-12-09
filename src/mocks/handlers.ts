d2a4b7fc11d3b36f88bbb74009d3aa5f4647fb0c
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-09 17:50:18 +0100

    Add addPhoto to handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,18 @@
 import { http, HttpResponse } from "msw";
-import { photosMock } from "./photosMock/photosMock";
+import { photoAddMock, photosMock } from "./photosMock/photosMock";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const handlers = [
   http.get(`${apiUrl}/photos`, async () => HttpResponse.json(photosMock)),
   http.delete(`${apiUrl}/photos/:_id`, async () => HttpResponse.json({})),
+  http.post(`${apiUrl}/photos/add`, async () =>
+    HttpResponse.json({ photo: photoAddMock[3] }),
+  ),
 ];
 
 export const errorHandlers = [
   http.get(`${apiUrl}/photos`, async () => HttpResponse.error()),
   http.delete(`${apiUrl}/photos/:_id`, async () => HttpResponse.error()),
+  http.post(`${apiUrl}/photos/add`, async () => HttpResponse.error()),
 ];
